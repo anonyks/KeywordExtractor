f618407e62d55224f1e59094d7c2c20da5b9d756
Kishan Shah <81799475+anonyks@users.noreply.github.com>
2024-09-19 17:09:52 +0545

    Add files via upload

---
+++ ext[Bulk].bat
@@ -0,0 +1,19 @@
+@echo off
+echo made by @ANONYKS_XD
+
+set "dir=%~dp0"
+echo Directory: %dir%
+
+echo Enter keyword to search:
+set /p keyword="> "
+
+echo =======================Start Sorting=============================================
+for /r %dir% %%f in (*.txt) do (
+  FINDSTR /L "%keyword%" "%%f" >> list.txt
+)
+echo =======================Combo Save========================================
+echo =======================Sort Complete=============================================
+
+pause
+
+@del /q temp.txt
